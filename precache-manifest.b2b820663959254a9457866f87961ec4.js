4604557ac69fe7e893bad8165b21b390753307d3
Mona Lisa <om.sergiy@gmail.com>
2019-11-08 11:02:00 +0200

    Updates

---
+++ precache-manifest.b2b820663959254a9457866f87961ec4.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0d014c44988b77559480203598c57a1",
+    "url": "/git-lesson/index.html"
+  },
+  {
+    "revision": "731c70ed8b438891dfd6",
+    "url": "/git-lesson/static/css/3.40f475da.chunk.css"
+  },
+  {
+    "revision": "cd3898c87b45a18cc304",
+    "url": "/git-lesson/static/css/4.82704a31.chunk.css"
+  },
+  {
+    "revision": "9901a2e6d419942b1afa",
+    "url": "/git-lesson/static/css/main.6ac72de3.chunk.css"
+  },
+  {
+    "revision": "b107cb7524f9fff21956",
+    "url": "/git-lesson/static/js/2.9357f9cd.chunk.js"
+  },
+  {
+    "revision": "731c70ed8b438891dfd6",
+    "url": "/git-lesson/static/js/3.0adad266.chunk.js"
+  },
+  {
+    "revision": "cd3898c87b45a18cc304",
+    "url": "/git-lesson/static/js/4.6631ca4e.chunk.js"
+  },
+  {
+    "revision": "9901a2e6d419942b1afa",
+    "url": "/git-lesson/static/js/main.c92100ab.chunk.js"
+  },
+  {
+    "revision": "ba86050488e37869444c",
+    "url": "/git-lesson/static/js/runtime-main.c826ecea.js"
+  },
+  {
+    "revision": "7c4b2dc81bdbad689884b44f0eabf9e7",
+    "url": "/git-lesson/static/media/avatarImg.7c4b2dc8.png"
+  },
+  {
+    "revision": "01ef99b894baf1594b4014cec43fbc21",
+    "url": "/git-lesson/static/media/preloader.01ef99b8.gif"
+  }
+]);
